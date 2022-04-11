7dc0c4557a7d0a2333198244e5f07abbc6b4b2e7
Stephane Burwash <stephane.burwash@potloc.com>
2022-04-11 10:47:24 -0400

    Added auth information

--- tap_hubspot/tap.py
+++ tap_hubspot/tap.py
@@ -22,31 +22,18 @@ class TapHubspot(Tap):
     """Hubspot tap class."""
     name = "tap-hubspot"
 
-    # TODO: Update this section with the actual config values you expect:
     config_jsonschema = th.PropertiesList(
         th.Property(
-            "auth_token",
-            th.StringType,
-            required=True,
-            description="The token to authenticate against the API service"
-        ),
-        th.Property(
-            "project_ids",
+            "access_token",
             th.ArrayType(th.StringType),
             required=True,
-            description="Project IDs to replicate"
+            description="PRIVATE Access Token for Hubspot API (TODO: Add public option)",
         ),
         th.Property(
             "start_date",
             th.DateTimeType,
             description="The earliest record date to sync"
-        ),
-        th.Property(
-            "api_url",
-            th.StringType,
-            default="https://api.mysample.com",
-            description="The url for the API service"
-        ),
+        )
     ).to_dict()
 
     def discover_streams(self) -> List[Stream]:
