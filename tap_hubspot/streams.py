d0f736275f47446133a44cc8967927d87109c44c
Stephane Burwash <stephane.burwash@potloc.com>
2022-04-11 16:11:25 -0400

    Made owners pagination work

--- tap_hubspot/streams.py
+++ tap_hubspot/streams.py
@@ -1,23 +1,43 @@
 """Stream type classes for tap-hubspot."""
+import requests
+import singer
 
 from pathlib import Path
 from typing import Any, Dict, Optional, Union, List, Iterable
 
 from singer_sdk import typing as th  # JSON Schema typing helpers
+from pathlib import Path
+from typing import Any, Dict, Optional, Union, List, Iterable
+
+from memoization import cached
+
+from singer_sdk.helpers.jsonpath import extract_jsonpath
+from singer_sdk.streams import RESTStream
+from singer_sdk.authenticators import BearerTokenAuthenticator
 
 from tap_hubspot.client import HubspotStream
 SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
 
-
+LOGGER = singer.get_logger()
 class OwnersStream(HubspotStream):
     """Define custom stream."""
+    _LOG_REQUEST_METRIC_URLS=True
     name = "owners"
     path = "/crm/v3/owners"
     primary_keys = ["id"]
     replication_key = "updatedAt"
     records_jsonpath = "$.results[*]"
-    next_page_token_jsonpath = "$.paging.next.link"
-    # Optionally, you may also use `schema_filepath` in place of `schema`:
+    next_page_token_jsonpath = "$.paging.next.after"
+
+    def get_url_params(
+        self, context: Optional[dict], next_page_token: Optional[Any]
+    ) -> Dict[str, Any]:
+        """Return a dictionary of values to be used in URL parameterization."""
+        params: dict = {}
+        LOGGER.debug(f"DEBUG: {params}")
+        if next_page_token:
+            params["after"] = next_page_token
+        return params
 
 
 
