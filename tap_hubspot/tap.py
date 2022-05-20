871ac4160c7cd54c14bfe7fb149533ef41439c85
Stephane Burwash <stephane.burwash@potloc.com>
2022-05-20 13:56:58 -0400

    Remove archived streams

--- tap_hubspot/tap.py
+++ tap_hubspot/tap.py
@@ -6,9 +6,6 @@ from typing import List
 from singer_sdk import Tap, Stream
 from singer_sdk import typing as th  # JSON schema typing helpers
 from tap_hubspot.streams import (
-    ArchivedCompaniesStream,
-    ArchivedContactsStream,
-    ArchivedDealsStream,
     AssociationsDealsToCompaniesStream,
     ContactsStream ,
     CompaniesStream,
@@ -22,9 +19,6 @@ from tap_hubspot.streams import (
 )
 
 STREAM_TYPES = [
-    ArchivedCompaniesStream,
-    ArchivedContactsStream,
-    ArchivedDealsStream,
     AssociationsDealsToCompaniesStream,
     ContactsStream,
     CompaniesStream,
